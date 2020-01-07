a087e3d19d26a917151104a58b11159c2752cbb3
wanmaoor <wanmao1284879375@outlook.com>
2020-01-07 16:59:42 +0800

    函数接口

--- function.ts
+++ function.ts
@@ -11,3 +11,13 @@ console.log(sum(1, 23))
 const mySum: (x: number, y: number) => number = (a, b) => a + b
 
 console.log(mySum(2, 2))
+
+// interface
+
+interface ISum {
+  (x: number, y: number): number
+}
+
+const iSum: ISum = (m, n) => m + n
+
+console.log(iSum(3,4))

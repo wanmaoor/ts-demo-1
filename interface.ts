e201b22f2f3e44d51783d88d73b255a89832504c
wanmaoor <wanmao1284879375@outlook.com>
2020-01-07 15:38:55 +0800

    接口

---
+++ interface.ts
@@ -0,0 +1,11 @@
+interface IPerson {
+  name: string;
+  age: number;
+}
+
+const man: IPerson = {
+  name: "wanmao",
+  age: 18
+}
+
+console.log(man)
---
+++ primitive.ts
@@ -0,0 +1,7 @@
+let notANumber: number = NaN;
+let infinityNumber: number = Infinity;
+let str: string;
+str = 'wanmao'
+console.log(str)
+console.log(notANumber)
+console.log(infinityNumber)

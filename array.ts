0df9d1eb1e023bff09568fe2aa6c933947cf3105
wanmaoor <wanmao1284879375@outlook.com>
2020-01-07 16:46:43 +0800

    类数组

--- array.ts
+++ array.ts
@@ -6,3 +6,17 @@ interface IStudents {
 let people: Array<IStudents> = [{name: 'alice', gender: false}, {name: 'Bob', gender: true}]
 
 console.log(people)
+
+// 类数组
+
+function sum(a,b,c) {
+  let sum: number = 0
+  let args: IArguments = arguments;
+  for (let i: number = 0; i < args.length; i++) {
+    sum += args[i]
+  }
+  return sum
+}
+
+const res: number = sum(1,2,3)
+console.log(res)

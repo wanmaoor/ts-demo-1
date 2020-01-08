3d388e47cc17a5cac78eba6cda6e53206809e491
wanmaoor <wanmao1284879375@outlook.com>
2020-01-08 19:23:07 +0800

    多个泛型参数互相约束

--- template.ts
+++ template.ts
@@ -33,5 +33,20 @@ function loggingIdentity<T extends Length>(arg: T): T {
   return arg
 }
 
-loggingIdentity('wanmao')
-loggingIdentity([1,2,3])
+loggingIdentity("wanmao")
+loggingIdentity([1, 2, 3])
+
+// 多个泛型参数之间互相约束
+function copyFields<T extends U, U>(target: T, source: U): T {
+  for (let id in source) {
+    if (source.hasOwnProperty(id)) {
+      target[id] = (<T>source)[id]
+    }
+  }
+  return target
+}
+
+let x = {a: 1, b: 2, c: 3, d: 4}
+
+copyFields(x, {b: 10, d: 20})
+console.log(x) // { a: 1, b: 10, c: 3, d: 20 }
